eec50ffa4bfab39a67faf5eb2056647066b4c7b1
Mark <barrantes_marco@yahoo.com>
2022-02-17 13:57:53 -0500

    authentication route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs/dist/bcrypt')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult } = require('express-validator')
+
+const User = require('../models/User')
 
 /**
  *      @route      GET api/auth
@@ -15,8 +21,51 @@ router.get('/', (req, res) => {
  *      @desc       Auth user & get token
  *      @access     Public
  */
-router.post('/', (req, res) => {
-    res.send('Log in user')
+router.post('/', [
+    check('email', 'Please include a valid email!').isEmail(),
+    check('password', 'Password is required!').exists()
+],async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body
+
+    try {
+        let user = await User.findOne({ email })
+
+        // if invalid email
+        if(!user) {
+            return res.status(400).json({ msg: 'Invalid Credentials!' })
+        }
+
+        // check the password
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        // if invalid password
+        if(!isMatch) {
+            return res.status(400).json({ msg: 'Invalid Credentials!' })
+        }
+
+        // json web-token, set the payload
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        // json web-token, sign the web-token
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 3600 // expires in one hour
+        }, (err, token) => {
+            if(err) throw err
+            res.json({ token })
+        })
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 module.exports = router
