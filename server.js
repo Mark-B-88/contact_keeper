0fe728874bb899ae9e62563f3cc34ad67ca313d3
Mark <barrantes_marco@yahoo.com>
2022-02-22 12:07:36 -0500

    prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 
@@ -8,16 +9,17 @@ connectDB()
 // Middleware
 app.use(express.json({ extended: false }))
 
-// Routes
-app.get('/', (req,res) => {
-    res.json({
-        msg: 'Welcome to the Contact Keeper API...'
-    })
-})
-
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
+
+// Serve Static Assets in Production
+if(process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000
 
