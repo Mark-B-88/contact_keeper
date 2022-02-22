4e6efbd60b2fecf284ab95161edb8eb0c4155efa
Mark <barrantes_marco@yahoo.com>
2022-02-22 11:27:17 -0500

    get & clear contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,19 +1,26 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import ContactItem from './ContactItem'
+import Spinner from '../layout/Spinner'
 import ContactContext from '../../context/contact/contactContext'
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext)
 
-    const { contacts, filtered } = contactContext
+    const { contacts, filtered, getContacts, loading } = contactContext
 
-    if(contacts.length === 0){
+    useEffect(() => {
+        getContacts()
+        // eslint-disable-next-line
+    },[])
+
+    if(contacts !== null && contacts.length === 0 && !loading){
         return <h4>Please add a contact</h4>
     }
 
     return (
         <>
+            {contacts !== null && !loading ? (
             <TransitionGroup>
                 {filtered !== null 
                     ? filtered.map(contact => (
@@ -35,6 +42,7 @@ const Contacts = () => {
                         </CSSTransition>
                     ))}
             </TransitionGroup>
+            ) : <Spinner />}
         </>
     )
 }
